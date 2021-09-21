75b91533a54bed901b9efe15ce99fc9f1091cb3e
Daisuke Kondo <daisuke.kondo@smarthr.co.jp>
2021-09-22 03:33:53 +0900

    🛠 functionalize the main proc

--- main.js
+++ main.js
@@ -7,38 +7,42 @@ Array.prototype.uniq = function () {
   return [...new Set(this)]
 }
 
-const fileNames = fs
-  .readdirSync(DIR_PATH, { withFileTypes: true })
-  .map(dirent => dirent.name.split('.')[0])
-  .filter(v => v)
+const main = () => {
+  const fileNames = fs
+    .readdirSync(DIR_PATH, { withFileTypes: true })
+    .map(dirent => dirent.name.split('.')[0])
+    .filter(v => v)
 
-const allDeps = fileNames
-  .map(fileName => require(`${DIR_PATH}/${fileName}.json`))
-  .map(json => ([
-    ...Object.entries(json.dependencies),
-    ...Object.entries(json.devDependencies)
-  ]))
+  const allDeps = fileNames
+    .map(fileName => require(`${DIR_PATH}/${fileName}.json`))
+    .map(json => ([
+      ...Object.entries(json.dependencies),
+      ...Object.entries(json.devDependencies)
+    ]))
 
-const libraryNames = allDeps
-  .flat()
-  .map(dep => dep[0])
-  .uniq()
-  .filter(libraryName => !libraryName.startsWith('@types/'))
-  .sort()
+  const libraryNames = allDeps
+    .flat()
+    .map(dep => dep[0])
+    .uniq()
+    .filter(libraryName => !libraryName.startsWith('@types/'))
+    .sort()
 
-const header = ['library', ...fileNames]
+  const header = ['library', ...fileNames]
 
-const table = [
-  header,
-  ...libraryNames.map(libraryName => [
-    libraryName,
-    ...allDeps.map(deps => {
-      const dep = deps.find(dep => dep[0] === libraryName)
-      return dep?.[1] ?? ''
-    }),
-  ]),
-]
+  const table = [
+    header,
+    ...libraryNames.map(libraryName => [
+      libraryName,
+      ...allDeps.map(deps => {
+        const dep = deps.find(dep => dep[0] === libraryName)
+        return dep?.[1] ?? ''
+      }),
+    ]),
+  ]
 
-const csv = parse(table, { header: false })
+  const csv = parse(table, { header: false })
 
-fs.writeFileSync('result.csv', csv)
+  fs.writeFileSync('result.csv', csv)
+}
+
+main()
