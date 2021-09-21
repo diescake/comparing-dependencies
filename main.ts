3dda954cb6035d9658e44b6bc08af18422760916
Daisuke Kondo <daisuke.kondo@smarthr.co.jp>
2021-09-22 07:25:05 +0900

    🔧 obey TypeScript

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ import { parse } from 'json2csv'
 
 const uniq = (arr: string[]) => [...new Set(arr)]
 
-export const extractLibraryNames = (allDeps: Array<Array<[string, unknown]>>) => {
+const extractLibraryNames = (allDeps: Array<Array<[string, unknown]>>) => {
   const _libraryNames = allDeps.flat().map(dep => dep[0])
 
   return uniq(_libraryNames)
@@ -11,7 +11,10 @@ export const extractLibraryNames = (allDeps: Array<Array<[string, unknown]>>) =>
 }
 
 export const createCsv = (packageJsons: Array<Record<string, unknown>>) => {
-  const allDeps = packageJsons.map(json => [...Object.entries(json.dependencies), ...Object.entries(json.devDependencies)])
+  const allDeps = packageJsons.map(json => [
+    ...Object.entries(json.dependencies as Record<string, string>),
+    ...Object.entries(json.devDependencies as Record<string, string>),
+  ])
   const libraryNames = extractLibraryNames(allDeps)
 
   const fileNames = packageJsons.map(json => json.name) as string[]
